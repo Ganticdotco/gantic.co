ac2454fb2f0a2efd7c2c1e398081b75564aa3ab6
Jai Mitchell <jai@gantic.co>
2017-05-07 16:04:49 +1000

    Contact form toggle highlights contact info

--- js/script.js
+++ js/script.js
@@ -6,14 +6,19 @@ function Scripticles(){}
 
   [].forEach.call(document.getElementsByClassName('toggleForm'), function(el) {
     el.addEventListener('click', function() {
-      var formo = document.getElementById('overlay-contact-form')
-      formo.classList.toggle('active')
+      var formo = document.getElementById('overlay-contact-form');
+      formo.classList.toggle('active');
+      var highlights = document.getElementsByClassName('highlight');
+      var highlightCount = highlights.length;
+      for (var i = 0; i < highlightCount; i++) {
+        highlights[i].classList.toggle('highlighted');
+      }
     })
   })
 
   window.onload = function() {
-    var content = document.getElementById('container')
-    content.classList.toggle('preload')
+    var content = document.getElementById('container');
+    content.classList.toggle('preload');
   }
 
 }).apply(Scripticles);
